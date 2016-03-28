4fbb2fc31c22fb4743aa40708de5e22407da385f
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-03-27 18:18:56 -0600

    update files

---
+++ operadores.lua
@@ -0,0 +1,68 @@
+--lua solo permite operadores aritmeticos entre valores enteros y decimales
+b = true;
+c = 'c';
+i = 1;
+d = 10.05;
+s = "hola";
+-------------------------------------
+--operador  + (suma)
+-------------------------------------
+--attempt to perform arithmetic on global 'b' (a boolean value)
+--print(b+b); 
+--print(b+c); 
+--print(b+i);
+--print(b+d);
+--print(b+s);
+--print(i+b);
+--attempt to perform arithmetic on global 'c' (a string value)
+--print(c+b);
+--print(c+i);
+--print(c+d);
+--print(c+s);
+--print(i+c);
+--print(i+s);
+--print(s+b);
+--print(s+c);
+--print(s+i);
+--print(s+s);
+print("suma");
+print(i+i); --2
+print(i+d); --11.05
+print(d+i); --11.05
+print(d+d); --20.1 
+
+-------------------------------------
+--operador  - (resta)
+-------------------------------------
+print("resta");
+print(i-i); --0
+print(i-d); --(-9.05)
+print(d-i); --9.05
+print(d-d); --0 
+
+-------------------------------------
+--operador  * (multiplicacion)
+-------------------------------------
+print("multiplicacion");
+print(i*i); --1
+print(i*d); --10.05
+print(d*i); --10.05
+print(d*d); --101.0025 
+
+-------------------------------------
+--operador  / (division)
+-------------------------------------
+print("division");
+print(i/i); --1
+print(i/d); --0.099502487562189
+print(d/i); --10.05
+print(d/d); --1
+
+-------------------------------------
+--operador  % (modulo)
+-------------------------------------
+print("modulo");
+print(i%i); --0
+print(i%d); --1
+print(d%i); --0.050000000000001
+print(d%d); --0
---
+++ variables.lua
@@ -0,0 +1,17 @@
+--declaracion de variables basicas --salida
+l=true;
+print(l); --true
+e=10; 
+print(e); --10
+d=1.5;
+print(d); --1.5
+lit='esta \t es una \n literal';
+print(lit); --esta   es una
+            --literal
+s="esta \t es \nuna cadena"; --esta    es
+           --una cadena 
+print(s);
+PI = 3.14;
+print(PI);
+PI = 3.1416;
+print(PI); --esta no es tomada como constante    
