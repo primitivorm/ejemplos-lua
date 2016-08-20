76493b4799aa5634b6868fe9bf47d9e86a283d2e
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-08-20 08:52:10 -0500

    se modifica listas.lua

--- listas.lua
+++ listas.lua
@@ -12,12 +12,16 @@ lista = {
 }
 
 texto = ''
-        for k,enlista in pairs(lista) do --[[ aquí empieza transformando la lista a array declarandola como "enlista"
+--[[ aquí empieza transformando la lista a array declarandola como "enlista"
 arriba está indicando que leerá el valor "lista" ]]
-                if leer == enlista then  -- si leer es igual al valor transformado a array
-                print("Esta en la lista :D") -- imprimirá esto, del caso contrario, no imprimirá nada
-                end
-
+for k,enlista in pairs(lista) do
+        if leer == enlista then  -- si leer es igual al valor transformado a array
+        print(leer .. " esta en la lista :D") -- imprimirá esto, del caso contrario, no imprimirá nada
+        lista[k] = "xxxxxx"   --si esta en la lista modificas el valor
         end
+end
 
-
+print("Ahora la lista contiene:")
+for k,enlista in pairs(lista) do
+  print(lista[k])
+end
